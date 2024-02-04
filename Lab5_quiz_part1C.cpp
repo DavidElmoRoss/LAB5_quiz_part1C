e218b144e19621bf19d7e480985eba3dd26240a1
David Elmo Ross <110311948+DavidElmoRoss@users.noreply.github.com>
2024-02-04 15:08:28 -0500

    Update Lab5_quiz_part1C.cpp

--- Lab5_quiz_part1C.cpp
+++ Lab5_quiz_part1C.cpp
@@ -21,7 +21,7 @@ Grove_LCD_RGB_Backlight lcd(D14,D15);
 
 int main(void)
 {
-    Carmine.attach(&count,2);                            // YOUR STATEMENT goes on THIS LINE
+                                // YOUR STATEMENT goes on THIS LINE
     lcd.clear();
     lcd.setRGB(0xFF,0x00,0x00);
     for(;;)
